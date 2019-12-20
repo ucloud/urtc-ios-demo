e2c6da6de14f397ba33c5399d5f185323be933e8
tony.jing <tony.jing@ucloud.cn>
2019-12-20 17:51:08 +0800

    添加IM demo

---
+++ UCloudIM-demo/UCloudIMSdk-demo/UCloudIMSdk-demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UCloudIMSdk-demo
+//
+//  Created by Tony on 2019/12/4.
+//  Copyright © 2019 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ UCloudIM-demo/UCloudIMSdk-demo/UCloudIMSdk-demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UCloudIMSdk-demo
+//
+//  Created by Tony on 2019/12/4.
+//  Copyright © 2019 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
