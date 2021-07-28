d0b3db4b23c4a316b9594090305c49926e473f20
ucloudyj <yj.wang@ucloud.cn>
2021-07-28 12:37:51 +0800

    提交demo

---
+++ PLVLiveScenesDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  PLVLiveEcommerceDemo
+//
+//  Created by Lincal on 2020/4/29.
+//  Copyright © 2020 PLV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ PLVLiveScenesDemo/Demo/Bugly/PLVBugReporter.h
@@ -0,0 +1,28 @@
+//
+//  PLVBugReporter.h
+//  PLVLiveScenesDemo
+//
+//  Created by MissYasiky on 2020/12/25.
+//  Copyright © 2020 PLV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSInteger, PLVBuglyBundleType) {
+    PLVBuglyBundleTypeWatch = 0,
+    PLVBuglyBundleTypeStreamer,
+};
+
+@interface PLVBugReporter : NSObject
+
+/// 启动异常上报功能，默认不启动
++ (void)openWithType:(PLVBuglyBundleType)type;
+
+/// 使用用户的 viewerID 设置用户标识
++ (void)setUserIdentifier:(NSString *)userId;
+
+@end
+
+NS_ASSUME_NONNULL_END
